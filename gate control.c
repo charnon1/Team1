9656fdd366117329a542e9c22698b5c759d51e50
waityaw <fangcong@me.com>
2016-05-11 17:20:39 +1200

    Update and rename Gate control to gate control.c

--- gate control.c
+++ gate control.c
@@ -10,9 +10,9 @@ int main (){
    // everything is working correctly
    init(1);
    //connects to server with the ip address 130.195.6.196
-   connect_to_server("130.195.6.196");
+   connect_to_server("130.195.6.196",1024);
    //sends a message to the connected server
-   send_to_server("Please open gate");
+   send_to_server("Please");
    //receives message from the connected server
    char message[24];
    receive_from_server(message); //this may be buggy!
