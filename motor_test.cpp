98e803ba4a80eb6107a57b5e7815fc94c35fcdcf
Abyss-Wanderer <Abyss-Wanderer@users.noreply.github.com>
2016-05-13 10:21:23 +1200

    Update motor_test.cpp
    Changed so that the file will now test different speeds, in forward and reverse, and turns.

--- motor_test.cpp
+++ motor_test.cpp
@@ -5,17 +5,75 @@ extern "C" int init(int d_lev);
 extern "C" int Sleep( int sec , int usec );
 extern "C" int set_motor(int motor , int speed );
 int main (){
-   // This sets up the RPi hardware and ensures
-   // everything is working correctly
-   init(0);
-   // Sets the motor connected to pin 1 to rotate
-   // in one direction at MAX speed .
-   set_motor(1, 255);
-   // Sets the motor connected to pin 2 to rotate
-   // in the opposite direction at 50 % speed .
-   set_motor(2, 255);
-   Sleep(5 ,000000);
-   // Sets both motors to 0
-   set_motor(1,0);
-   set_motor(2,0);
+// This sets up the RPi hardware and ensures
+// everything is working correctly
+init(0);
+// Sets both motors to rotate at 100% speed (255), then in reverse.
+set_motor(1, 255);
+set_motor(2, 255);
+Sleep(1, 0);
+set_motor(1, -255);
+set_motor(2, -255);
+Sleep(1, 0);
+set_motor(1, 0);
+set_motor(2, 0);
+Sleep(4, 0);
+//75% speed (191)
+set_motor(1, 191);
+set_motor(2, 191);
+Sleep(2, 0);
+set_motor(1, -191);
+set_motor(2, -191);
+Sleep(2, 0);
+set_moto(1, 0);
+set_motor(2, 0);
+Sleep(4, 0);
+//50% speed (127)
+set_motor(1, 127);
+set(motor(2, 127);
+Sleep(3, 0);
+set_motor(1, -127);
+set_motor(2, -127);
+Sleep(3, 0);
+set_motor(1, 0);
+set_motor(2, 0);
+Sleep(4, 0);
+//25% speed (56)
+set_motor(1, 56);
+set_motor(2, 56);
+Sleep(4, 0);
+set_motor(1, -56);
+set_motor(2, -56);
+Sleep(4, 0);
+set_motor(1, 0);
+set_motor(2, 0);
+//Attempts to turn on the spot with both motors in full speed in opposite directions
+set_motor(1, 255);
+set_motor(2, -255);
+Sleep(1, 0);
+set_motor(1, 0);
+set_motor(2, 0);
+Sleep(4, 0);
+//Turns at 75%
+set_motor(1, -191);
+set_motor(2, 191);
+Sleep(2, 0);
+set_motor(1, 0);
+set_motor(2, 0);
+Sleep(4, 0);
+//Turns at 50%
+set_motor(1, 127);
+set_motor(2, -127);
+Sleep(3, 0);
+set_motor(1, 0);
+set_motor)2, 0);
+Sleep(4,0);
+//Turns at 25%
+set_motor(1, -56);
+set_motor(2, 56);
+sleep(4, 0);
+//sets motors to 0
+set_motor(1, 0);
+set_motor(2, 0);
+
 return 0;}
