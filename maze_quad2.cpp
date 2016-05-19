61f9d249199a429dba41df9f7175cf086fdf06db
Abyss-Wanderer <Abyss-Wanderer@users.noreply.github.com>
2016-05-19 15:27:19 +1200

    Update maze_quad2.cpp
    Updated to be the code on the pi we wrote on wednesday (18/5/2016).
    Does not work: current known error is that the set_motor() commands refuse the value passed through them (say it needs to be within the range of -255 to 255).

--- maze_quad2.cpp
+++ maze_quad2.cpp
@@ -1,3 +1,4 @@
+
 //Including libraries
 #include <stdio.h>
 #include <time.h>
@@ -8,34 +9,64 @@ extern "C" int Sleep(int sec, int usec);
 extern "C" int set_motor(int motor, int speed);
 extern "C" int connect_to_server(char server_addr[15], int port);
 extern "C" int send_to_server(char message[24]);
-extern "C" int recieve_from_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+extern "C" int take_picture();
+extern "C" char get_pixel(int row,int col,int colour);
+
+//Defining constants
+double const kp = 0.5;
+double const kd = 0.5;
+int const base_speed = 80;
+int const delay_s = 0;
+int const delay_us = 250000;
 
 int main(){
 
-	//This sets up the RPi hardware and ensures that everything is working correctly
-	init(1);
+        double current_error = 0;
+        double proportional_signal;
+        double error;
 
-	//Connects to the gate server
-	conect_to_server("130.195.6.196", 1024);
+        //This sets up the RPi hardware and ensures that everything is working correctly
+        init(1);
 
-	//Sends the message "Please" to the gate server, which makes the server send the password to the Pi
-	send_to_server("Please");
+        //Connects to the gate server
+//      connect_to_server("130.195.6.196", 1024);
 
-	//Recieves the password from the server and stores it in an array of chars
-	char message[24];
-	recieve_from_server(message);
-	send_to_server(message);
+        //Sends the message "Please" to the gate server, which makes the server send the pas$
+//      send_to_server("Please");
 
-	//Assumes that the server opened the gate within a second and drives forward at FULL SPEED
-	//I have no idea if this is long enough to drive through the gate or short enough to stop the Pi before the next quadrant, so we should find out the optimum time to run the motors at full power for
-	Sleep(1,0);
-	set_motor(1,255);
-	set_motor(2,255);
-	Sleep(2,0);
+        //Recieves the password from the server and stores it in an array of chars
+//      char message[24];
+//      receive_from_server(message);
+//      send_to_server(message);
 
-	//Sets both motors to 0
-	set_motor(1,0);
-	set_motor(1,0);
+        //Loops for taking and analysing pictures to get errors. Runs 30 times
+        for(int n=0; n<30; n++){
+                take_picture();
 
-	return 0;
+                //calculates proportional error
+                for (int i=0; i<320; i++){
+                error = (i-160)*get_pixel(120, i, 3);
+                        current_error = current_error + error;
+                }
+                //calculates derivative error
+
+                //implements errors to control motors
+                proportional_signal = current_error*kp;
+
+                printf("Proportional signal is: %d\n", proportional_signal );
+
+                set_motor(1, base_speed - proportional_signal);
+                set_motor(2, base_speed + proportional_signal);
+
+                //sleeps for the defined delays
+                Sleep(delay_s, delay_us);
+        }
+
+        //Sets both motors to 0
+        set_motor(1, 0);
+
+        set_motor(2, 0);
+
+        return 0;
 }
